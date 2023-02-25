cfafa249e9c04624f067cb4dfebf906de1debefc
Nigel <nbazzeghin@gmail.com>
2023-02-25 01:18:46 -0600

    fix: build script package version now syncs to nomad version (#3)
    INFO file and Nomad bin properly replaced in final package, Nomad to version 1.4.4

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-export PACKAGE_VERSION=1.4.3-2000
+export NOMAD_VERSION="${NOMAD_VERSION:-1.4.4}"
+export PACKAGE_VERSION="${NOMAD_VERSION}-1000"
 export OS="${OS:-linux}"
 export ARCH="${ARCH:-amd64}"
-export NOMAD_VERSION="${NOMAD_VERSION:-1.4.3}"
 nomad_zip_file="nomad_${NOMAD_VERSION}_${OS}_${ARCH}.zip"
 export NOMAD_URL="${NOMAD_URL:-https://releases.hashicorp.com/nomad/${NOMAD_VERSION}/${nomad_zip_file}}"
 
@@ -14,11 +14,9 @@ if [[ ! -f "./tmp/${nomad_zip_file}" ]]; then
     curl --create-dirs -Lo "./tmp/$nomad_zip_file" "${NOMAD_URL}"
 fi
 
-if [[ ! -f "./package/bin/nomad" ]]; then
-    unzip -o -d "./package/bin/" "./tmp/${nomad_zip_file}"
-fi
+ unzip -o -d "./package/bin/" "./tmp/${nomad_zip_file}"
 
-./info.sh "${PACKAGE_VERSION}" >> INFO
+./info.sh "${PACKAGE_VERSION}" > INFO
 tar -cvzf package.tgz package
 tar -cvf package.spk INFO LICENSE conf/ package.tgz scripts/pre* scripts/post* scripts/start*
 rm package.tgz
