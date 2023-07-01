9f4a1330d308535271b906f0b881b1f33f04adbb
Prabir Shrestha <mail@prabir.me>
2023-07-01 13:31:05 -0700

    generate nomad cert

---
+++ WIZARD_UIFILES/install_uifile.sh
@@ -0,0 +1,71 @@
+#!/bin/sh
+
+SYNOLOGY_IP=$(ip route get "$(ip route show 0.0.0.0/0 | grep -oP 'via \K\S+')" | grep -oP 'src \K\S+')
+
+tee "$SYNOPKG_TEMP_LOGFILE" <<EOF
+[
+    {
+	"step_title": "Nomad Configuration",
+	"items": [
+	    {
+		"type": "textfield",
+		"desc": "IP address of Nomad Server (Default: Synology primary ip)",
+		"subitems": [
+		    {
+			"key": "pkgwizard_ip",
+			"desc": "IP of your synology device",
+			"defaultValue": "$SYNOLOGY_IP",
+			"validator": {
+			    "allowBlank": false
+			}
+		    }
+		]
+	    },
+	    {
+		"type": "textfield",
+		"desc": "Port for Nomad Web UI",
+		"subitems": [
+		    {
+			"key": "pkgwizard_nomad_web_ui_port",
+			"desc": "Port for Nomad Web UI",
+			"defaultValue": "4646",
+			"validator": {
+			    "allowBlank": false
+			}
+		    }
+		]
+	    },
+	    {
+		"type": "textfield",
+		"desc": "Nomad Region",
+		"subitems": [
+		    {
+			"key": "pkgwizard_nomad_region",
+			"desc": "Nomad Region",
+			"defaultValue": "global",
+			"validator": {
+			    "allowBlank": false
+			}
+		    }
+		]
+	    },
+	    {
+		"type": "textfield",
+		"desc": "Nomad Datacenter",
+		"subitems": [
+		    {
+			"key": "pkgwizard_nomad_dc",
+			"desc": "Nomad Datacenter",
+			"defaultValue": "dc1",
+			"validator": {
+			    "allowBlank": false
+			}
+		    }
+		]
+	    }
+	]
+    }
+];
+EOF
+
+exit 0
